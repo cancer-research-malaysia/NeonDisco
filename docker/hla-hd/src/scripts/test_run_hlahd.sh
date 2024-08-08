029126f9650f6d59317f7beacfdf669260937656
sufyazi <sufyazi@outlook.com>
2024-08-09 00:14:06 +0800

    finish batch script for hlahd

--- docker/hla-hd/src/scripts/test_run_hlahd.sh
+++ docker/hla-hd/src/scripts/test_run_hlahd.sh
@@ -6,49 +6,66 @@ OUTDIR="/work/out"
 SAMT-OUT="${OUTDIR}/samtools-out"
 mkdir ${SAMT-OUT}
 
-# SamToFastq I=merged.bam F=merged_R1.fq F2=merged_R2.fq
-# awk '{if(NR%4 == 1){O=$0; gsub("/1","1",O); print O}else{print $0}}' merged_R1.fq > ${sample-id}-MHC_WES-b2f-R1.fq
-# awk '{if(NR%4 == 1){O=$0; gsub("/2","2",O); print O}else{print $0}}' merged_R2.fq > ${sample-id}-MHC_WES-b2f-R2.fq
-# # execution of HLA-HD (4 threads, discard reads <30bp)
-# hlahd.sh -t 4 -m 30 -f ${hlahd-dir}/freq_data/ ${sample-id}-MHC_WES-b2f-R1.fq ${sample-id}-MHC_WES-b2f-R2.fq ${hlahd-
-# dir}/HLA_gene.split.3.50.0.txt ${hlahd-dir}/dictionary/ ${sample-id}_WES-MHC-bam ${outdir} >> log.txt 2>&1
-
 #################################################
 samtools --help
 picard SamToFastq --help
+hlahd.sh --help
 
 # This script is used to run HLA-HD for multiple samples in a loop
 # first search the data space for unique bam files
-export BAMS=($(find "${TSVINPUT}" -mindepth 1 -maxdepth 1 -type f -name '*.fq.gz'))
+export BAMS=($(find "${TSVINPUT}" -mindepth 1 -maxdepth 1 -type f -name '*.bam'))
 echo "Found ${#BAMS[@]} BAM files in the input directory."
 
-# Loop through each BAM file
-for SAMPLE in "${BAMS[@]}"; do
-    # Extract the sample name from the file path
-    SAMPLE_NAME=$(basename "${SAMPLE}" .fq.gz)
-    echo "Processing sample: ${SAMPLE_NAME}"
-    
+# Check if the bam array is empty or not
+if [ ${#BAMS[@]} -eq 0 ]; then
+    echo "No BAM files found in the input directory. Exiting."
+    exit 1
+else
+    # Loop through each BAM file
+    for SAMPLE in "${BAMS[@]}"; do
+        # Extract the sample name from the file path
+        SAMPLE_NAME=$(basename "${SAMPLE}" .bam)
+        echo "Processing sample: ${SAMPLE_NAME}"
+        # # first preprocess WES data
+        # # use "chr6:28477797-33448354" for hg19
+        # samtools view -b -h "$SAMPLE" "chr6:28510120-33480577" > "${SAMT-OUT}/MHC-${SAMPLE_NAME}.bam" 
+        # samtools view -b -f 4 "$SAMPLE" > "${SAMT-OUT}/unmapped-${SAMPLE_NAME}.bam"
+        # samtools merge -o "${SAMT-OUT}/merged-${SAMPLE_NAME}.bam" "${SAMT-OUT}/MHC-${SAMPLE_NAME}.bam" "${SAMT-OUT}/unmapped-${SAMPLE_NAME}.bam"
 
-    # first preprocess WES data
-    # use "chr6:28477797-33448354" for hg19
-    # samtools view -b -h "$SAMPLE" "chr6:28510120-33480577" > "${SAMT-OUT}/MHC-${SAMPLE_NAME}.bam" 
-    # samtools view -b -f 4 "$SAMPLE" > "${SAMT-OUT}/unmapped-${SAMPLE_NAME}.bam"
-    # samtools merge -o "${SAMT-OUT}/merged-${SAMPLE_NAME}.bam" "${SAMT-OUT}/MHC-${SAMPLE_NAME}.bam" "${SAMT-OUT}/unmapped-${SAMPLE_NAME}.bam"
+        # # run Picard conversion
+        # if picard SamToFastq I="${SAMT-OUT}/merged-${SAMPLE_NAME}.bam" F="${SAMT-OUT}/merged-${SAMPLE_NAME}_R1.fq" F2="${SAMT-OUT}/merged-${SAMPLE_NAME}_R2.fq"; then
+        #     echo "Picard conversion successful for sample ${SAMPLE_NAME}."
+        #     # relabel records with awk
+        #     awk '{if(NR%4 == 1){O=$0; gsub("/1","1",O); print O}else{print $0}}' "${SAMT-OUT}/merged-${SAMPLE_NAME}_R1.fq" > "${OUTDIR}/merged-${SAMPLE_NAME}-MHC-WES-b2f-R1.fq" && \
+        #     awk '{if(NR%4 == 1){O=$0; gsub("/2","2",O); print O}else{print $0}}' "${SAMT-OUT}/merged-${SAMPLE_NAME}_R2.fq" > "${OUTDIR}/merged-${SAMPLE_NAME}-MHC-WES-b2f-R2.fq"
+            
+        #     HLAHD_DIR="/tmp/hlahd.1.7.0"
+        #     # check R1 and R2 fq file existence 
+        #     if [ -f "${OUTDIR}/merged-${SAMPLE_NAME}-MHC-WES-b2f-R1.fq" ] && [ -f "${OUTDIR}/merged-${SAMPLE_NAME}-MHC-WES-b2f-R2.fq" ]; then
+        #         echo "Running HLA-HD for sample ${SAMPLE_NAME}."
+                
+        #         mkdir -p "${OUTDIR}/logs/"
 
+        #         # measure execution time
+        #         STARTTIME=$(date +%s)
 
-#     if [ -f "${SAMPLE}/arriba-fusions.tsv" ]; then
-#         echo "Fusion file found for sample ${SAMPLE_NAME}."
-#         # Run AGFusion on the sample
-#         echo "Running AGFusion for sample ${SAMPLE_NAME}..."
-#         if agfusion batch -f "${SAMPLE}/arriba-fusions.tsv" -a ${TOOL} -db "${AGF_DB}/agfusion.homo_sapiens.95.db" -o "${OUTDIR}/agf_output_${SAMPLE_NAME}_${TOOL}" --middlestar --noncanonical; then
-#             echo "AGFusion run successfully for sample ${SAMPLE_NAME}."
-#         else
-#             echo "AGFusion run failed for sample ${SAMPLE_NAME}."
-#         fi
-#     else 
-#         echo "Fusion file not found for sample ${SAMPLE_NAME}. Skipping."
-#         continue
-#     fi
-done
-
-
+        #         # run HLA-HD (8 threads, discard reads <30bp)
+        #         if hlahd.sh -t 8 -m 30 -f ${HLAHD_DIR}/freq_data/ "${OUTDIR}/merged-${SAMPLE_NAME}-MHC-WES-b2f-R1.fq" "${OUTDIR}/merged-${SAMPLE_NAME}-MHC-WES-b2f-R2.fq" "${HLAHD_DIR}/HLA_gene.split.3.50.0.txt" "${HLAHD_DIR}/dictionary/" "${OUTDIR}/${SAMPLE_NAME}_WES-MHC-bam" ${OUTDIR} 2>&1 | tee "${OUTDIR}/logs/${SAMPLE_NAME}-HLAHD-run.log-$(date +%Y%m%d_%H-%M-%S).txt"; then
+        #             echo "HLA-HD run successfully for sample ${SAMPLE_NAME}."
+        #             ENDTIME=$(date +%s)
+        #             ELAP=$(( ENDTIME - STARTTIME ))
+        #             echo "Time taken: ${ELAP}. Check log file for run details."
+        #         else
+        #             echo "HLA-HD run failed for sample ${SAMPLE_NAME}."
+        #             exit 1
+        #         fi
+        #     else
+        #         echo "One or both FASTQ files are missing for sample ${SAMPLE_NAME}."
+        #         exit 1
+        #     fi
+        # else
+        #     echo "Picard conversion failed for sample ${SAMPLE_NAME}."
+        #     exit 1
+        # fi
+    done
+fi
