e557ce75c0ecd152b72919211b68a7a914dcbc3e
Suffian Azizan <sufyazi@outlook.com>
2024-12-19 22:30:28 +0800

    finish star-align preprocessing script

--- bin/star-align-nf.sh
+++ bin/star-align-nf.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/bash
 
-SAMPLE_ID="${READ1%%_*}"
-CTAT_LIB="/work/libs"
-INPUTDIR="/work/input"
-OUTDIR="/work/nf_work"
+echo $(id)
 
-STAR \
+READ1=$1
+READ2=$2
+SAMPLE_ID="${READ1%%_*}"
+CTAT_LIB="/work/lib"
+OUTDIR="/work/output"
+
+if STAR \
 --runMode alignReads \
 --runThreadN 16 \
 --genomeDir "${CTAT_LIB}/ref_genome.fa.star.idx" \
---readFilesIn "${INPUTDIR}"/12T_R1.fq "${INPUTDIR}"/12T_R2.fq \
+--readFilesIn "${READ1}" "${READ2}" \
+--readFilesCommand gunzip -c \
 --outSAMtype BAM SortedByCoordinate \
 --outSAMunmapped Within \
 --limitBAMsortRAM 20000000000 \
 --outFileNamePrefix "${OUTDIR}/${SAMPLE_ID}-STAR_" \
---genomeLoad NoSharedMemory >> mapping.log.txt 2>&1
-
-# samtools index Aligned.sortedByCoord.out.bam -@ 16 -o Aligned.sortedByCoord.out.bai
-
-#/home/suffian/libs/GRCh38_gencode_v37_CTAT_lib_Mar012021.plug-n-play/ctat_genome_lib_build_dir/ref_genome.fa.star.idx
+--genomeLoad NoSharedMemory >> "${OUTDIR}/mapping.log.txt" 2>&1; then
+    samtools index "${OUTDIR}/${SAMPLE_ID}-STAR_Aligned.sortedByCoord.out.bam" -@ 16 -o "${OUTDIR}/${SAMPLE_ID}-STAR_Aligned.sortedByCoord.out.bai" && echo "STAR alignment complete."
+fi
 
