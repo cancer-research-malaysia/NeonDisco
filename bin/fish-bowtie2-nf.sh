46783f837bf5cfff43cf6032e2c2129096c3c82b
Suffian Azizan <sufyazi@outlook.com>
2025-02-27 12:44:35 +0800

    fix boolean logic

--- bin/fish-bowtie2-nf.sh
+++ bin/fish-bowtie2-nf.sh
@@ -16,7 +16,7 @@ mkdir -p "bowtie2-o"
 if bowtie2 -p ${CORES} -x "${BT_IDX}" -1 "${FQ_INPUT1}" -2 "${FQ_INPUT2}" -S "${SAMPLE_ID}.hlamap.sam"; then
     echo "HLA-loci read mapping for sample ${SAMPLE_ID} is complete."
     # extract mapped reads
-    samtools view -h -F 4 "${SAMPLE_ID}.hlamap.sam" > "${SAMPLE_ID}.MAPPED.sam" && echo "Mapped reads extracted." || echo "Mapped reads extraction failed." && exit 1
+    samtools view -h -F 4 "${SAMPLE_ID}.hlamap.sam" > "${SAMPLE_ID}.MAPPED.sam" && echo "Mapped reads extracted." || (echo "Mapped reads extraction failed." && exit 1)
 else
     echo "HLA-loci read mapping for sample ${SAMPLE_ID} failed."
     exit 1
