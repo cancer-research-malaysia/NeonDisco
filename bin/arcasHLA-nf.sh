dc3f49a52c25aab6fa99a96e441170cfd916ec58
Suffian Azizan <sufyazi@outlook.com>
2025-03-04 14:05:49 +0800

    squash bug

--- bin/arcasHLA-nf.sh
+++ bin/arcasHLA-nf.sh
@@ -12,22 +12,14 @@ echo "Number of cores to use: ${CORES}"
 STARTTIME=$(date +%s)
 
 # run arcasHLA extract command
-arcasHLA extract ${BAM} -t ${CORES} -v 
+arcasHLA extract ${BAM} -t ${CORES} -v && arcasHLA genotype *.1.fq.gz *.2.fq.gz -t ${CORES} -v
 
-if [ -f *.extracted.1.fq.gz && -f *.extracted.2.fq.gz ]; then
-    echo "arcasHLA extract run successfully for sample ${SAMPLE_ID}."
-    arcasHLA genotype *.1.fq.gz *.2.fq.gz -t ${CORES} -v
-    if [ $? -eq 0 ]; then
-        echo "arcasHLA run successfully for sample ${SAMPLE_ID}."
-        ENDTIME=$(date +%s)
-        echo "arcasHLA run time: $(($ENDTIME - $STARTTIME)) seconds"
-    else
-        echo "arcasHLA failed for sample ${SAMPLE_ID}."
-        exit 1
-    fi
+if [ $? -eq 0 ]; then
+    echo "arcasHLA run successfully for sample ${SAMPLE_ID}."
+    ENDTIME=$(date +%s)
+    echo "arcasHLA run time: $(($ENDTIME - $STARTTIME)) seconds"
 else
-    echo "arcasHLA extract failed for sample ${SAMPLE_ID}."
+    echo "arcasHLA failed for sample ${SAMPLE_ID}."
     exit 1
 fi
-
 
