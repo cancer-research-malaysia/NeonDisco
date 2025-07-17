10ff5ddbe48f3f167e32a4af7a1ad8307c7868a8
Suffian Azizan <sufyazi@outlook.com>
2025-07-17 11:52:27 +0000

    squash bugs

--- bin/fuscat--nf.sh
+++ bin/fuscat--nf.sh
@@ -3,26 +3,23 @@
 # get script arguments
 READ1=$1
 READ2=$2
-SAMPLE_ID="${READ1%%_*}"
 DB=$3
 CORE=$4
+SAMPLE_ID=$5
 
 run_fuscat() {
   local FASTQ_R1_FILE=$1
   local FASTQ_R2_FILE=$2
   local DB=$3
   local CORES=$4
-  local OUTDIR=$5
+  local SAMPLE=$5
 
-  fusioncatcher -d ${DB} --input ${FASTQ_R1_FILE},${FASTQ_R2_FILE} --output ${OUTDIR} -p ${CORES} 2>&1 | tee "${OUTDIR}/fuscat-run.log-$(date +%Y%m%d_%H-%M-%S).txt"
+  fusioncatcher -d ${DB} --input ${FASTQ_R1_FILE},${FASTQ_R2_FILE} --output ${SAMPLE} -p ${CORES} 2>&1 | tee "${SAMPLE}/fuscat-run.log-$(date +%Y%m%d_%H-%M-%S).txt"
 }
-
-# Set variables
-OUTDIR="${SAMPLE_ID}"
 
 # measure execution time
 STARTTIME=$(date +%s)
-if run_fuscat "${READ1}" "${READ2}" "${DB}" "${CORE}" "${OUTDIR}"; then
+if run_fuscat "${READ1}" "${READ2}" "${DB}" "${CORE}" "${SAMPLE_ID}"; then
     ENDTIME=$(date +%s)
     ELAP=$(( ENDTIME - STARTTIME ))
     echo "FusionCatcher run completed successfully. Time taken: ${ELAP}. Check log file for run details."
